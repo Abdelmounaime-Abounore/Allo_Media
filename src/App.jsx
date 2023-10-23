d500f8c2dd181e3baa700a039990c2485a850ac9
Elite_Book <abdelmounaim,abounore@gmail.com>
2023-10-23 23:32:05 +0100

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Authentification from './Component/Auth'
+
+function App() {
+
+  return (
+    <div>
+      <Authentification />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Auth/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+
+const Authentification = () => {   
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">ffff</label>
+                <input type="" />
+            </form>
+        </div>
+    );
+};
+
+export default Authentification;
