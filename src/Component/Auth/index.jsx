cdce96c1af61eca62833637ff60a8317e806333c
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-24 09:21:08 +0100

    create authentification component

--- src/Component/Auth/index.jsx
+++ src/Component/Auth/index.jsx
@@ -1,12 +1,75 @@
-import "./index.css"
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import './index.css'
 
-const Authentification = () => {   
+const Authentification = () => {
+    const [action, setAction] = useState("Register")
     return (
-        <div>
-            <form action="">
-                <label htmlFor="">ffff</label>
-                <input type="" />
-            </form>
+        <div className='container'>
+            <div className='header'>
+                <div>{action}</div>
+                <div className='underline'></div>
+            </div>
+            <div className='form'>
+                {action === "Login" ?
+                    <>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faUser} />
+                            <input type="text" placeholder='User Name .. ' />
+                        </div>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faLock} />
+                            <input type="password" placeholder='Password .. ' />
+                        </div>
+                    </> :
+                    <>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faUser} />
+                            <input type="text" placeholder='User Name .. ' />
+                        </div>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faLock} />
+                            <input type="password" placeholder='Password .. ' />
+                        </div>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                            <input type="email" placeholder='Email .. ' />
+                        </div>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faPhone} />
+                            <input type="text" placeholder='Phone .. ' />
+                        </div>
+                        <div className='inputs'>
+                            <FontAwesomeIcon icon={faLocationDot} />
+                            <input type="text" placeholder='Adress .. ' />
+                        </div>
+                        <div className='input-img'>
+                            <FontAwesomeIcon icon={faImage} />
+                            <input type="file" />
+                        </div>
+                    </>
+                }
+
+            </div>
+            <div className='buttons'>
+                <div className='submits'>
+                    <div className={action === "Login" ? "state" : "submit"} onClick={() => { setAction("Register") }}>Register</div>
+                    <div className={action === "Register" ? "state" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+                </div>
+                {action === "Register" ?
+                    <></> :
+                    <div>
+                        <div className="forgot-password">Forgot Password</div>
+                        {/* <div className='underline'></div> */}
+                    </div>
+                }
+            </div>
         </div>
     );
 };
