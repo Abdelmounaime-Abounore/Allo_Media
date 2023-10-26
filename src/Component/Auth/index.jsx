386268ea87e9dcd4725f98829789c4b6a7b5b516
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-10-26 11:10:01 +0100

    finalize the register part

--- src/Component/Auth/index.jsx
+++ src/Component/Auth/index.jsx
@@ -9,6 +9,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faImage } from '@fortawesome/free-solid-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 import './index.css'
 
@@ -25,6 +26,9 @@ const Authentification = () => {
         roleName: ''
     })
 
+    const [successMessage, setSuccessMessage] = useState("");
+
+
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData({
@@ -40,22 +44,40 @@ const Authentification = () => {
             const response = await axios.post('http://localhost:9000/api/auth/register', formData);
 
             console.log('Response Data:', response.data); 
+
+            setSuccessMessage("Registration successful!");
+
+            setTimeout(() => {
+                setSuccessMessage(""); 
+            }, 5000); 
+
+
+            setFormData({
+                name: "",
+                password: "",
+                email: "",
+                phoneNumber: "",
+                address: "",
+                roleName: "",
+                image: ""
+            });
         } catch (error) {
             console.error('Error:', error); 
         }
     }
 
-    const handleLoginSubmit = async (e) => {
+    // const handleLoginSubmit = async (e) => {
         // e.preventDefault();
 
-        try {
-            const response = await axios.post('http://localhost:9000/api/auth/login', formData);
+    //     try {
+    //         const response = await axios.post('http://localhost:9000/api/auth/login', formData);
 
-            console.log('Response Data:', response.data); 
-        } catch (error) {
-            console.error('Error:', error); 
-        }
-    };
+    //         console.log('Response Data:', response.data); 
+    //     } catch (error) {
+    //         console.error('Error:', error); 
+    //     }
+    // };
+    
 
     const schema = yup.object().shape({
         name: yup.string().required("*Your name is Required"),
@@ -66,101 +88,107 @@ const Authentification = () => {
         .required(),
         address: yup.string().required("*Your adress is required"),
         roleName: yup.string().required("*Invalid role"),
-        image: yup.mixed().test({
-            name: 'fileTypeAndSize',
-            message: 'Invalid file format or file is too large',
-            test: (value) => {
-                if (!value) return true;
-    
-                const validTypes = ["image/jpeg", "image/png", "image/gif"];
-                const maxSize = 5242880; // 5MB
-    
-                return validTypes.includes(value.type) && value.size <= maxSize;
-            },
-        }),
+    //     image: yup
+    // .mixed()
+    // .required("*Image field is required")
+    // .test('fileType', 'Invalid file format', function (value) {
+    //   if (!value) return true;
+
+    //   const validTypes = ['image/jpeg', 'image/png', 'image/gif'];
+    //   return validTypes.includes(value.type);
+    // }),
     })
-    
+
     const { register, handleSubmit, formState: {errors}} = useForm({
         resolver: yupResolver(schema),
     })
 
     return (
-        <div className='container'>
-            <div className='header'>
-                <div>{action}</div>
-            </div>
-            <form onSubmit={handleSubmit(handleRegisterSubmit)}>
-                <div className='form'>
-                    {action === "Login" ?
-                        <>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faUser} />
-                                <input type="email" name='email' {...register("email")} placeholder='Email .. ' />
-                            </div>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faLock} />
-                                <input type="password" name='password' {...register("password")} placeholder='Password .. ' />
-                            </div>
-                        </> :
-                        <>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faUser} />
-                                <input type="text" name='name' placeholder='User Name .. ' value={formData.name} {...register("name")} onChange={handleInputChange}  />
-                            </div>
-                            <p>{errors.name?.message}</p>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faLock} />
-                                <input type="password" name='password' placeholder='Password .. ' value={formData.password} {...register("password")} onChange={handleInputChange} />
-                            </div>
-                            <p>{errors.password?.message}</p>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faEnvelope} />
-                                <input type="email" name='email' placeholder='Email .. ' value={formData.email} {...register("email")} onChange={handleInputChange} />
-                            </div>
-                            <p>{errors.email?.message}</p>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faPhone} />
-                                <input type="text" name='phoneNumber' placeholder='Phone .. ' value={formData.phoneNumber} {...register("phoneNumber")} onChange={handleInputChange} />
-                            </div>
-                            <p>{errors.phoneNumber?.message}</p>
-                            <div className='inputs'>
-                                <FontAwesomeIcon icon={faLocationDot} />
-                                <input type="text" name='address' placeholder='Adress .. ' value={formData.address} {...register("address")} onChange={handleInputChange} />
-                            </div>
-                            <p>{errors.address?.message}</p>
-                            <div className='options'>
-                                {/* <FontAwesomeIcon icon={faLocationDot} /> */}
-                                {/* <input type="text" name='roleName' placeholder='Role .. ' value={formData.roleName} onChange={handleInputChange} /> */}
-                                <select name='roleName' value={formData.roleName} {...register("roleName")} onChange={handleInputChange} >
-                                    <option value="">Role .. </option>
-                                    <option value="client">Client</option>
-                                    <option value="livreur">Livreur</option>
-                                </select>
-                            </div>
-                            <p>{errors.roleName?.message}</p>
-                            <div className='input-img'>
-                                <FontAwesomeIcon icon={faImage} />
-                                <input name='image' type="file" value={formData.image} {...register("image")} onChange={handleInputChange} />
-                            </div>
-                            <p>{errors.image?.message}</p>
+        <div>
+            {
+                successMessage && 
+                <div className='success-message'>
+                    <FontAwesomeIcon  className="msg" icon={faCheck} />
+                    <div>{successMessage}</div>
+                </div>
+            }
+            <div className='container'>
+                <div className='header'>
+                    <div>{action}</div>
+                </div>
+                <form onSubmit={handleSubmit(handleRegisterSubmit)}>
+                    <div className='form'>
+                        {action === "Login" ?
+                            <>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faUser} />
+                                    <input type="email" name='email' {...register("email")} placeholder='Email .. ' />
+                                </div>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faLock} />
+                                    <input type="password" name='password' {...register("password")} placeholder='Password .. ' />
+                                </div>
+                            </> :
+                            <>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faUser} />
+                                    <input type="text" name='name' placeholder='User Name .. ' value={formData.name} {...register("name")} onChange={handleInputChange}  />
+                                </div>
+                                <p>{errors.name?.message}</p>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faLock} />
+                                    <input type="password" name='password' placeholder='Password .. ' value={formData.password} {...register("password")} onChange={handleInputChange} />
+                                </div>
+                                <p>{errors.password?.message}</p>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faEnvelope} />
+                                    <input type="email" name='email' placeholder='Email .. ' value={formData.email} {...register("email")} onChange={handleInputChange} />
+                                </div>
+                                <p>{errors.email?.message}</p>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faPhone} />
+                                    <input type="text" name='phoneNumber' placeholder='Phone .. ' value={formData.phoneNumber} {...register("phoneNumber")} onChange={handleInputChange} />
+                                </div>
+                                <p>{errors.phoneNumber?.message}</p>
+                                <div className='inputs'>
+                                    <FontAwesomeIcon icon={faLocationDot} />
+                                    <input type="text" name='address' placeholder='Adress .. ' value={formData.address} {...register("address")} onChange={handleInputChange} />
+                                </div>
+                                <p>{errors.address?.message}</p>
+                                <div className='options'>
+                                    {/* <FontAwesomeIcon icon={faLocationDot} /> */}
+                                    {/* <input type="text" name='roleName' placeholder='Role .. ' value={formData.roleName} onChange={handleInputChange} /> */}
+                                    <select name='roleName' value={formData.roleName} {...register("roleName")} onChange={handleInputChange} >
+                                        <option value="">Role .. </option>
+                                        <option value="client">Client</option>
+                                        <option value="livreur">Livreur</option>
+                                    </select>
+                                </div>
+                                <p>{errors.roleName?.message}</p>
+                                <div className='input-img'>
+                                    <FontAwesomeIcon icon={faImage} />
+                                    <input name='image' type="file" value={formData.image} onChange={handleInputChange} required />
+                                </div>
+                                {/* <p>{errors.image?.message}</p> */}
 
-                        </>
-                    }
-                </div>
-                <div className='buttons'>
-                    {action === "Register" ?
-                        <></> :
-                        <div>
-                            <div className="forgot-password">Forgot Password</div>
-                            {/* <div className='underline'></div> */}
-                        </div>
-                    }
-                    <div className='submits'>
-                        <button type='submit' className={action === "Login" ? "state" : "submit"} onClick={() => { setAction("Register") }}>Register</button>
-                        <div className={action === "Register" ? "state" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+                            </>
+                        }
                     </div>
-                </div>
-            </form>
+                    <div className='buttons'>
+                        {action === "Register" ?
+                            <></> :
+                            <div>
+                                <div className="forgot-password">Forgot Password</div>
+                                {/* <div className='underline'></div> */}
+                            </div>
+                        }
+                        <div className='submits'>
+                            <button type='submit' className={action === "Login" ? "state" : "submit"} onClick={() => { setAction("Register") }}>Register</button>
+                            <div className={action === "Register" ? "state" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
